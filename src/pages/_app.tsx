5246c87d7540f7206f790012e3fb787d144ffe81
iwa <iwaongit@gmail.com>
2024-03-17 20:22:47 +0100

    chore: 🗑️ clean

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,5 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
-
-import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
@@ -12,17 +8,12 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <SessionProvider session={session}>
-      <main className={`font-sans ${inter.variable}`}>
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
+    <main className={`font-sans ${inter.variable}`}>
+      <Component {...pageProps} />
+    </main>
   );
 };
 
-export default api.withTRPC(MyApp);
+export default MyApp;
